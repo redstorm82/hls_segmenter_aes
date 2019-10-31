caf801e10d0af13a647624a4d06df31be879b9a5
redstorm82 <redstorm82@users.noreply.github.com>
2019-10-31 21:21:06 +0800

    change ffmpeg libs dep

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf segmenter
-gcc -Wall -g -I/usr/include/ffmpeg/include aes.c m3u8-segmenter.c -o segmenter -L/usr/lib64 -lavformat
+gcc -Wall -g -I/usr/include/ffmpeg aes.c m3u8-segmenter.c -o segmenter -L/usr/lib64 -lavformat
 cp -f segmenter ../
