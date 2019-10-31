a0e601f6b5a7572b16e835a2d2e5f3ddc361f2a3
redstorm82 <redstorm82@users.noreply.github.com>
2019-10-31 21:19:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf segmenter
-gcc -Wall -g -I/usr/local/ffmpeg/include aes.c m3u8-segmenter.c -o segmenter -L/usr/local/ffmpeg/lib -lavformat
+gcc -Wall -g -I/usr/include/ffmpeg/include aes.c m3u8-segmenter.c -o segmenter -L/usr/lib64 -lavformat
 cp -f segmenter ../
